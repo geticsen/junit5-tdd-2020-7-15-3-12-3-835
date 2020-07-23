5280acc1ba6ed9c63f9591b0b4b48eb6930fba6d
ZHOUGR2 <286348794@qq.com>
2020-07-23 12:04:56 +0800

    [FizzBu​​zzWhizz] [Green] test:add should_return_whizz_when_play_fizz_buzz_whizz_given_7.

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -28,11 +28,21 @@ public class FizzBuzzWhizzTest {
     @Test
     void should_return_buzz_when_play_fizz_buzz_whizz_given_5(){
         //given
-        int number = 3;
+        int number = 5;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
         Assertions.assertEquals("buzz",actual);
+    }
+    @Test
+    void should_return_whizz_when_play_fizz_buzz_whizz_given_7(){
+        //given
+        int number = 7;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        //when
+        String actual = fizzBuzzWhizz.play(number);
+        //then
+        Assertions.assertEquals("whizz",actual);
     }
 }
