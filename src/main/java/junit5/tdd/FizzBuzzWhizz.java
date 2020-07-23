98eb4e4af495d68a9fca516a6f8e3d14bb2e17b2
ZHOUGR2 <286348794@qq.com>
2020-07-23 12:29:24 +0800

    [FizzBu​​zzWhizz] [Green] feature:implement FizzBuzzWhizz play function when given 7 return whizz.

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -1,12 +1,15 @@
 package junit5.tdd;
 
-public class FizzBuzzWhizz{
-    public String play(int number){
-        if(number % 3 == 0){
-            return  "fizz";
+public class FizzBuzzWhizz {
+    public String play(int number) {
+        if (number % 3 == 0) {
+            return "fizz";
         }
-        if(number % 5 == 0) {
+        if (number % 5 == 0) {
             return "buzz";
+        }
+        if (number % 7 == 0) {
+            return "whizz";
         }
         return String.valueOf(number);
     }
