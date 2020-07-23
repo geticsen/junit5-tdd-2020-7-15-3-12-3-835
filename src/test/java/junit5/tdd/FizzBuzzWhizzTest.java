e203223d4aa231a2c14f62ffed5992fbef1fc228
ZHOUGR2 <286348794@qq.com>
2020-07-23 12:37:30 +0800

    [FizzBu​​zzWhizz] [Green] test:add should_return_fizzwhizz_when_play_fizz_buzz_whizz_given_21 test

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -6,53 +6,67 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzWhizzTest {
     @Test
-    void should_return_1_when_play_fizz_buzz_whizz_given_1(){
+    void should_return_1_when_play_fizz_buzz_whizz_given_1() {
         //given
         int number = 1;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
-        Assertions.assertEquals("1",actual);
+        Assertions.assertEquals("1", actual);
     }
+
     @Test
-    void should_return_fizz_when_play_fizz_buzz_whizz_given_3(){
+    void should_return_fizz_when_play_fizz_buzz_whizz_given_3() {
         //given
         int number = 3;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
-        Assertions.assertEquals("fizz",actual);
+        Assertions.assertEquals("fizz", actual);
     }
+
     @Test
-    void should_return_buzz_when_play_fizz_buzz_whizz_given_5(){
+    void should_return_buzz_when_play_fizz_buzz_whizz_given_5() {
         //given
         int number = 5;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
-        Assertions.assertEquals("buzz",actual);
+        Assertions.assertEquals("buzz", actual);
     }
+
     @Test
-    void should_return_whizz_when_play_fizz_buzz_whizz_given_7(){
+    void should_return_whizz_when_play_fizz_buzz_whizz_given_7() {
         //given
         int number = 7;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
-        Assertions.assertEquals("whizz",actual);
+        Assertions.assertEquals("whizz", actual);
     }
+
     @Test
-    void should_return_fizzbuzz_when_play_fizz_buzz_whizz_given_15(){
+    void should_return_fizzbuzz_when_play_fizz_buzz_whizz_given_15() {
         //given
         int number = 15;
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
         //when
         String actual = fizzBuzzWhizz.play(number);
         //then
-        Assertions.assertEquals("fizzbuzz",actual);
+        Assertions.assertEquals("fizzbuzz", actual);
+    }
+    @Test
+    void should_return_fizzwhizz_when_play_fizz_buzz_whizz_given_21() {
+        //given
+        int number = 21;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        //when
+        String actual = fizzBuzzWhizz.play(number);
+        //then
+        Assertions.assertEquals("fizzwhizz", actual);
     }
 }
